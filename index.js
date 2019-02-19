eff85ee85288485d45aecef884c99f379bcf8e11
croy <roy.christie95@gmail.com>
2019-02-19 15:39:41 -0500

    PUT function is working

--- index.js
+++ index.js
@@ -61,6 +61,26 @@ server.post('/api/zoos', async (req, res) => {
   }
 })
 
+// Update Zoo by ID
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
+    
+      if (count > 0){
+        const zoo = await db('zoos')
+          .where({ id: req.params.id })
+          .first();
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: 'Zoo not found to update '});
+      }
+  } catch(error){
+    res.status(500).json(error);
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
