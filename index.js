ddd35b0d3d32fa1baf0e2beacbebca8cbd15c58e
croy <roy.christie95@gmail.com>
2019-02-19 15:18:15 -0500

    Finished GET request, returns curly brackets

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ server.use(helmet());
 
 // endpoints here
 // GET Zoos
-server.get('/api/zoos');
+server.get('/api/zoos', async (req, res) => {
+  try{
+    const zoos = await db('zoos');
+    res.status(200).json(roles);
+  } catch(error){
+    res.status(500).json(error);
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
